e7784062b594fec5024c17f3200acb4a8cd96e4a
adistroe <97851663+adistroe@users.noreply.github.com>
2023-04-25 20:05:43 +0300

    Delete Topics directory

--- Topics/Extension functions/Delegation/src/Main.kt
+++
@@ -1,5 +0,0 @@
-//Do not fix code below 
-fun next(prev: Int): Int = prev * 1000 - 10
-
-//write your code here
-fun Int.nextValue() = this * 1000 - 10
--- Topics/Random/SumIterator/src/Main.kt
+++
@@ -1,9 +0,0 @@
-import kotlin.random.Random
-
-fun sumInts(): Int {
-    // write here
-    var sum = 0
-    for (number in 1..100)
-        sum += number
-    return sum
-}
